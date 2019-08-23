4f8db570ad5b84438bc379f72053d7becda8e537
joshuaj0421 <joshuaj0421@gmail.com>
2019-08-23 13:37:07 +0000

    Add my files from yesterday

---
+++ PracticeV2.java
@@ -0,0 +1,7 @@
+public class PracticeV2
+{
+    public static void main(String[] args)
+    {
+        System.out.println("I can still type stuff.");
+    }
+}
---
+++ PracticeV3.java
@@ -0,0 +1,7 @@
+public class PracticeV3
+{
+    public static void main(String[] args)
+    {
+        System.out.println("I can still do this.");
+    }
+}
