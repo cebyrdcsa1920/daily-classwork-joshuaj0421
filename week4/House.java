d20afebe0fb3f0405651e0001d7573dcaab359b0
joshuaj0421 <joshuaj0421@gmail.com>
2019-09-05 14:09:28 +0000

    Added House.java and HouseRunner.java

---
+++ week4/House.java
@@ -0,0 +1,23 @@
+public class House
+{
+    private boolean runningWater;
+    private boolean electricity;
+    private int numberOfStories;
+    private int numberOfBathrooms;
+    private int numberOfBedrooms;
+    private boolean isRented;
+    private String address;
+
+    public House()
+    {
+        runningWater = true;
+        electricity = true;
+        numberOfStories = 2;
+        numberOfBathrooms = 4;
+        numberOfBedrooms = 3;
+        isRented = false;
+        address = "2324 Address Ln.";
+    }
+
+
+}
---
+++ week4/HouseRunner.java
@@ -0,0 +1,8 @@
+public class HouseRunner
+{
+    public static void main(String[] args)
+    {
+        House coolHouse = new House();
+        System.out.println(coolHouse);
+    }
+}
