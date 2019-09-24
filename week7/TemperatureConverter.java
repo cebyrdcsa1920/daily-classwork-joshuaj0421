0e616a2b2c3ee6caa07b21c032fd27171c89fdc5
joshuaj0421 <joshuaj0421@gmail.com>
2019-09-24 14:14:41 +0000

    Added conversion method and main method

--- week7/TemperatureConverter.java
+++ week7/TemperatureConverter.java
@@ -14,20 +14,40 @@ public class TemperatureConverter
         if (isCelsius)
         {
             temperature = (temperature * 9 / 5) + 32;
+            isCelsius = false;
+        }
+    }
+
+    public void convertToC()
+    {
+        if (!isCelsius)
+        {
+            temperature = (temperature - 32) * 5 / 9;
+            isCelsius = true;
         }
     }
 
     public String toString()
     {
-        String result = "The current temperature is " + temperature + " degrees.";
+        String result = "The current temperature is " + temperature + " degrees";
         if (isCelsius)
         {
             result += " Celsius";
         }
         else
         {
-            result+= " Fahrenheit";
+            result += " Fahrenheit";
         }
         return result;
+    }
+
+    public static void main(String[] args)
+    {
+        TemperatureConverter myTemp = new TemperatureConverter(25.6);
+        System.out.println(myTemp);
+        myTemp.convertToF();
+        System.out.println(myTemp);
+        myTemp.convertToC();
+        System.out.println(myTemp);
     }
 }
