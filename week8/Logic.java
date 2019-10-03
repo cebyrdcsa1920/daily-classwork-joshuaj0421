a520e02e8c08522b94a580e86dfd32a9a987738a
joshuaj0421 <joshuaj0421@gmail.com>
2019-10-03 13:41:23 +0000

    Created Logic.java

---
+++ week8/Logic.java
@@ -0,0 +1,25 @@
+public class Logic
+{
+    public static void main(String[] args)
+    {
+        boolean isTired = true;
+        if (isTired)
+        {
+            goToSleep();
+        }
+        else
+        {
+            playVideoGames();
+        }
+
+        public static void goToSleep()
+        {
+            System.out.println("ZZZzzzzzz");
+        }
+
+        public static void playVideoGames()
+        {
+            System.out.println("Gaming")
+        }
+    }
+}
